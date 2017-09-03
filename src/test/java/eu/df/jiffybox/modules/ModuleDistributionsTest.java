2c5d6ac3927e14fa6986d7e6fceb57ce4c357cc8
Julian Schlichtholz <julian.schlichtholz@gmail.com>
2017-09-03 18:47:59 +0200

    Update license, gradle wrapper, dependencies and test cases

--- src/test/java/eu/df/jiffybox/modules/ModuleDistributionsTest.java
+++ src/test/java/eu/df/jiffybox/modules/ModuleDistributionsTest.java
@@ -1,5 +1,7 @@
 package eu.df.jiffybox.modules;
 
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.github.tomakehurst.wiremock.common.Json;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import eu.df.jiffybox.WireMockHelper;
 import eu.df.jiffybox.models.Distribution;
@@ -46,10 +48,10 @@ public class ModuleDistributionsTest {
         assertEquals(8, distributions.size());
 
         Distribution distribution1 = distributions.get("centos_6_0_64bit");
-        Distribution distribution2 = distributions.get("centos_7_1_64bit");
+        Distribution distribution2 = distributions.get("centos_7_64bit");
         Distribution distribution3 = distributions.get("debian_jessie_64bit");
-        Distribution distribution4 = distributions.get("fedora_24_64bit");
-        Distribution distribution5 = distributions.get("opensuse_13_64bit");
+        Distribution distribution4 = distributions.get("debian_stretch_64bit");
+        Distribution distribution5 = distributions.get("fedora_24_64bit");
         Distribution distribution6 = distributions.get("opensuse_42_64bit");
         Distribution distribution7 = distributions.get("ubuntu_14_4_lts_64bit");
         Distribution distribution8 = distributions.get("ubuntu_16_4_lts_64bit");
@@ -73,17 +75,17 @@ public class ModuleDistributionsTest {
 
         assertEquals(2048, distribution4.getMinDiskSizeMB());
         assertEquals("xen-pvops-x86_64", distribution4.getDefaultKernel());
-        assertEquals("Fedora 24 64-Bit", distribution4.getName());
+        assertEquals("Debian Stretch (9) 64-Bit", distribution4.getName());
         assertEquals("ro", distribution4.getRootdiskMode());
 
         assertEquals(2048, distribution5.getMinDiskSizeMB());
         assertEquals("xen-pvops-x86_64", distribution5.getDefaultKernel());
-        assertEquals("openSUSE 13.2 64-bit", distribution5.getName());
+        assertEquals("Fedora 24 64-Bit", distribution5.getName());
         assertEquals("ro", distribution5.getRootdiskMode());
 
         assertEquals(2048, distribution6.getMinDiskSizeMB());
         assertEquals("xen-pvops-x86_64", distribution6.getDefaultKernel());
-        assertEquals("openSUSE 42.1 64-Bit", distribution6.getName());
+        assertEquals("openSUSE 42.2 64-Bit", distribution6.getName());
         assertEquals("ro", distribution6.getRootdiskMode());
 
         assertEquals(2048, distribution7.getMinDiskSizeMB());
