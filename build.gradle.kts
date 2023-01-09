bdd52ff200d99914903965203c9ac79f1fb1ab0f
Julian Nodorp <julian.nodorp@mailbox.org>
2023-01-09 22:15:27 +0100

    ci: prepare publish to maven central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    id("java")
-    id("maven-publish")
-    id("jacoco")
-    id("com.github.ben-manes.versions") version "0.44.0"
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.15"
+	id("java")
+	id("maven-publish")
+	id("jacoco")
+	signing
+	id("com.github.ben-manes.versions") version "0.44.0"
+	id("com.github.nbaztec.coveralls-jacoco") version "1.2.15"
 }
 
 group = "io.github.jnodorp"
@@ -13,83 +14,100 @@ version = "1.0.1"
 
 description = """Java implementation of the JiffyBox API"""
 
+java {
+	withJavadocJar()
+	withSourcesJar()
+}
+
 publishing {
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/jnodorp/jiffybox")
-            credentials {
-                username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
-            }
-        }
-    }
+	repositories {
+		maven {
+			credentials {
+				val sonatypeUsername: String? by project
+				username = sonatypeUsername
 
-    publications {
-        register<MavenPublication>("gpr") {
-            from(components["java"])
+				val sonatypePassword: String? by project
+				password = sonatypePassword
+			}
 
-            pom {
-                licenses {
-                    license {
-                        name.set("The MIT License")
-                        url.set("https://opensource.org/licenses/MIT")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("jnodorp")
-                        name.set("Julian Nodorp")
-                        email.set("julian.nodorp@mailbox.org")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:git://github.com/jnodorp/jiffybox.git")
-                    developerConnection.set("scm:git:ssh://git@github.com:jnodorp/jiffybox.git")
-                    url.set("https://github.com/jnodorp/jiffybox")
-                }
-            }
-        }
-    }
+			url = if (version.toString().endsWith("-SNAPSHOT")) {
+				uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+			} else {
+				uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+			}
+		}
+	}
+
+	publications {
+		register<MavenPublication>("jiffybox") {
+			from(components["java"])
+
+			pom {
+				licenses {
+					license {
+						name.set("MIT")
+						url.set("https://opensource.org/licenses/MIT")
+					}
+				}
+				developers {
+					developer {
+						id.set("jnodorp")
+						name.set("Julian Nodorp")
+						email.set("julian.nodorp@mailbox.org")
+					}
+				}
+				scm {
+					connection.set("scm:git:https://github.com/jnodorp/jiffybox.git")
+					developerConnection.set("scm:git:ssh://git@github.com:jnodorp/jiffybox.git")
+					url.set("https://github.com/jnodorp/jiffybox/tree/main")
+				}
+			}
+		}
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 }
 
 dependencies {
-    implementation("io.github.openfeign:feign-jackson:12.1")
+	implementation("io.github.openfeign:feign-jackson:12.1")
 
-    testImplementation("com.github.tomakehurst:wiremock:2.27.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
-    testImplementation("org.slf4j:slf4j-log4j12:2.0.6")
+	testImplementation("com.github.tomakehurst:wiremock:2.27.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.1")
+	testImplementation("org.slf4j:slf4j-log4j12:2.0.6")
 
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.1")
 }
 
-tasks.withType<Jar> {
-    manifest {
-        attributes["Automatic-Module-Name"] = "${project.group}.${project.name}"
-    }
+signing {
+	// Configure by setting the ORG_GRADLE_PROJECT_signingSecretKey environment variable.
+	val signingSecretKey: String? by project
+
+	// Configure by setting the ORG_GRADLE_PROJECT_signingPassword environment variable.
+	val signingPassword: String? by project
+
+	useInMemoryPgpKeys(signingSecretKey, signingPassword)
+	sign(publishing.publications["jiffybox"])
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
-    }
+	useJUnitPlatform()
+	testLogging {
+		events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+	}
 }
 
 tasks.withType<JacocoReport> {
-    reports {
-        xml.apply {
-            required.set(true)
-        }
-    }
+	reports {
+		xml.apply {
+			required.set(true)
+		}
+	}
 }
 
 tasks.withType<Javadoc> {
-    setDestinationDir(file("$projectDir/docs"))
+	setDestinationDir(file("$projectDir/docs"))
 
-    (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+	(options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
 }
